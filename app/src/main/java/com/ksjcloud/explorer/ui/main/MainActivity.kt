f329bdf8f8d597f8790b57fe9a6771eb35373b21
cloudnkim <yesksj26@gmail.com>
2021-11-24 12:36:45 +0900

    프래그먼트 트랜잭션 추가

--- app/src/main/java/com/ksjcloud/explorer/ui/main/MainActivity.kt
+++ app/src/main/java/com/ksjcloud/explorer/ui/main/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.ksjcloud.explorer.R
 import com.ksjcloud.explorer.ui.base.baseActivity
 
 class MainActivity : baseActivity(), MainI.View {
     private val mPresenter:MainPresenter<MainI.View> = MainPresenter()
     private val mContext: Context = this
+
+    private var mFragmentManager:FragmentManager = supportFragmentManager
+    private var mFragmentTransaction:FragmentTransaction = mFragmentManager.beginTransaction()
 
     private lateinit var mFragmentArea:FrameLayout
 
@@ -19,6 +23,7 @@ class MainActivity : baseActivity(), MainI.View {
 
         mFragmentArea = findViewById(R.id.fragment_root)
         mPresenter.onAttach(mContext, this)
+
     }
 
     override fun onDestroy() {
