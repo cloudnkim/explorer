961467e49486127409dc5212331a7ddd15e34f5c
cloudnkim <yesksj26@gmail.com>
2021-11-26 00:31:49 +0900

    Fragment에 baseActivity 넘기도록 수정

--- app/src/main/java/com/ksjcloud/explorer/ui/base/BaseI.kt
+++ app/src/main/java/com/ksjcloud/explorer/ui/base/BaseI.kt
@@ -5,6 +5,7 @@ import android.content.Context
 interface BaseI {
     interface Presenter<V:View> {
         fun onAttach(context: Context, appView:V)
+        fun onAttach(context: Context, baseActivity: BaseActivity, appView:V)
         fun onDetach()
     }
 
--- app/src/main/java/com/ksjcloud/explorer/ui/base/BasePresenter.kt
+++ app/src/main/java/com/ksjcloud/explorer/ui/base/BasePresenter.kt
@@ -4,7 +4,16 @@ import android.content.Context
 
 open class BasePresenter<V : BaseI.View> : BaseI.Presenter<V> {
 
+    lateinit var mBaseActivity: BaseActivity
+    lateinit var mAppView:V
+
     override fun onAttach(context: Context, appView: V) {
+        mAppView = appView
+    }
+
+    override fun onAttach(context: Context, baseActivity: BaseActivity, appView: V) {
+        mBaseActivity = baseActivity
+        mAppView = appView
     }
 
     override fun onDetach() {
